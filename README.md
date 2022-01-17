b8a4b3f8268e8f06ebb7a11b3df1eda8f9c92a59
punkq <15528102019@163.com>
2022-01-17 19:55:22 +0800

    update md

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-# Wellcome
+# [Wellcome](#top)
 ## Interests
 - [biology](docs\interests\biology.md)
 - [phycology](docs\interests\phycology.md)
 ## Projects
 - [22-01-15-paper_reading](docs\projects\22-01-15-paper_reading.md)
 ## Skills
-- [algorithm](docs\skills\code\algorithm.md)
-- [python](docs\skills\code\python.md)
 - code
   - [algorithm](docs\skills\code\algorithm.md)
   - [python](docs\skills\code\python.md)
-  - [algorithm](docs\skills\code\algorithm.md)
-  - [python](docs\skills\code\python.md)
-### [Back to top](Welcome)
+### [Back to top](#top)
--- update.py
+++ update.py
@@ -4,34 +4,33 @@ import os
 def write_category(cat):
     strings = []
     strings.append('## {}'.format(cat.capitalize()))
+    cat_path = os.path.join('docs', cat)
 
-    for root, dirs, files in os.walk(os.path.join('docs', cat), topdown=False):
-        for name in files:
-            if not name.endswith('.md'): continue
-            file_path = os.path.join(root, name)
-            file_title = name[:-3]
-            strings.append('- [{}]({})'.format(file_title, file_path))
-        for dir_name in dirs:
-            strings.append('- {}'.format(dir_name))
-            for name in os.listdir(os.path.join(root, dir_name)):
-                if not name.endswith('.md'): continue
-                for name in os.listdir(os.path.join(root, dir_name)):
-                    file_path = os.path.join(root, dir_name, name)
-                    file_title = name[:-3]
-                    strings.append('  - [{}]({})'.format(file_title, file_path))
-
+    for name in os.listdir(cat_path):
+        if os.path.isfile(os.path.join(cat_path, name)):
+            if name.endswith('.md'): 
+                file_path = os.path.join(cat_path, name)
+                file_title = name[:-3]
+                strings.append('- [{}]({})'.format(file_title, file_path))
+        else:
+            strings.append('- {}'.format(name))
+            for subname in os.listdir(os.path.join(cat_path, name)):
+                if not subname.endswith('.md'): continue
+                file_path = os.path.join(cat_path, name, subname)
+                file_title = subname[:-3]
+                strings.append('  - [{}]({})'.format(file_title, file_path))
 
     return '\n'.join(strings)
 
 md = []
-title = '# Wellcome'
+title = '# [Wellcome](#top)'
 md.append(title)
 
 cats = os.listdir('docs')
 for cat in cats:
     md.append(write_category(cat))
 
-md.append('### [Back to top](Welcome)')
+md.append('### [Back to top](#top)')
 
 with open('README.md', 'w') as  f:
     f.write('\n'.join(md))
